e931e2d355bb487d6bd1163b6fe681a7b850cd52
roodhouse <rughjm@gmail.com>
2023-07-06 20:49:21 -0500

    some date styling

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -6,13 +6,13 @@ function Date() {
     <>
       <div id="dateContainer">
         <form>
-          <div id="inputContainer">
+          <div id="inputContainer" className='flex mb-[48px]'>
             <div id="dayContainer" className='mr-4'>
               <div id="dayLabel" className='font-["Poppins"] text-xs tracking-[3px] font-bold text-gray mb-1'>
                 <label htmlFor="day">DAY</label>
               </div>
               <div id="dayInput">
-                <input type="number" name='day' placeholder='DD' className='px-4 py-3 border border-solid border-line rounded-lg w-[87px]' />
+                <input type="number" name='day' placeholder='DD' className='px-4 py-3 border border-solid border-line rounded-lg w-[87px] text-black text-[20px] font-["Poppins"] font-bold tracking-[0.2px]' />
               </div>
             </div>
 
@@ -21,7 +21,7 @@ function Date() {
                 <label htmlFor="month">MONTH</label>
               </div>
               <div id="monthInput">
-                <input type="number" name='month' placeholder='MM' className='px-4 py-3 border border-solid border-line rounded-lg w-[87px]' />
+                <input type="number" name='month' placeholder='MM' className='px-4 py-3 border border-solid border-line rounded-lg w-[87px] text-black text-[20px] font-["Poppins"] font-bold tracking-[0.2px]' />
               </div>
             </div>
 
@@ -30,7 +30,7 @@ function Date() {
                 <label htmlFor="year">YEAR</label>
               </div>
               <div id="yearInput">
-                <input type="number" name='year' placeholder='YYYY' className='px-4 py-3 border border-solid border-line rounded-lg w-[87px]' />
+                <input type="number" name='year' placeholder='YYYY' className='pl-4 pr-0 py-3 border border-solid border-line rounded-lg w-[87px] text-black text-[20px] font-["Poppins"] font-bold tracking-[0.2px]' />
               </div>
             </div>
           </div>
