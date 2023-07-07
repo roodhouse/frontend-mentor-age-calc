9af122b992b7e319a9b3799af9581958541c3104
roodhouse <rughjm@gmail.com>
2023-07-07 08:56:07 -0500

    dt view done

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Output from './components/Output'
 function App() {
   return (
     <div className="App">
-     <div id='mainWrapper' className='bg-lightGray h-screen pt-[88px] px-4 pb-[238px]'>
-      <div id='mainContainer' className='bg-white rounded-[24px_24px_100px_24px] py-12 px-6'>
-        <div id='dateWrapper' className='mb-8'>
+     <div id='mainWrapper' className='flex justify-center items-center bg-lightGray h-screen pt-[88px] px-4 pb-[238px]'>
+      <div id='mainContainer' className='bg-white rounded-[24px_24px_100px_24px] py-12 px-6 max-w-[343px] lg:max-w-full lg:w-[840px] lg:p-[56px] lg:rounded-[24px_24px_200px_24px]'>
+        <div id='dateWrapper' className='mb-8 lg:mb-0'>
           <Date />
         </div>
         <div id='outputWrapper'>
